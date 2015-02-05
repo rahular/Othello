ff1a8a295487825ad7479b93c9be85c50eb656be
rahular <rahulife007@gmail.com>
2015-02-05 15:36:15 +0530

    A nothing commit for syncing

---
+++ AI.md
@@ -0,0 +1,3 @@
+## AI Design
+
+The initial approach is to build a shallow artificial neural netwrok and train it on raw board positions.
--- src/com/rahul/othello/players/AIPlayer.java
+++ src/com/rahul/othello/players/AIPlayer.java
@@ -13,18 +13,12 @@ import com.rahul.othello.Point;
  */
 public class AIPlayer implements Player {
 
-	/* (non-Javadoc)
-	 * @see com.rahul.othello.Player#nextMove(com.rahul.othello.Board)
-	 */
 	@Override
 	public Point nextMove(Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.rahul.othello.Player#getName()
-	 */
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
